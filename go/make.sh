ac0f061d996ef6b431c6300f561ecddf9c584802
Tanner Burns <tjburs102@gmail.com>
2018-10-29 20:17:30 -0500

    updated make.sh, default to all

--- go/make.sh
+++ go/make.sh
@@ -4,14 +4,26 @@ if [ $1 ]
 then
     if [ $1 == "init" ]
     then
+        echo "Building init.."
         go build src/init/init.go src/init/database.go src/init/logger.go
     elif [ $1 == "elk" ]
     then
+        echo "Building elk.."
         go build src/elk/elk.go src/elk/database.go src/elk/elkClient.go src/elk/indexer.go src/elk/logger.go
     elif [ $1 == "api" ]
     then
+        echo "Building api.."
         go build src/api/api.go src/api/database.go src/api/model.go src/api/logger.go
-    else
-        echo "No valid arguments found"
     fi
+    echo "Done!"
+else
+    echo "Building all"
+    echo "########################"
+    echo "Building init.."
+    go build src/init/init.go src/init/database.go src/init/logger.go
+    echo "Building elk.."
+    go build src/elk/elk.go src/elk/database.go src/elk/elkClient.go src/elk/indexer.go src/elk/logger.go
+    echo "Building api.."
+    go build src/api/api.go src/api/database.go src/api/model.go src/api/logger.go
+    echo "Done!"
 fi
